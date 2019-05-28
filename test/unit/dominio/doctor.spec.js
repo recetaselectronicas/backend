3b6eacacb52bc1ef8420743e2d3380f0b106dfef
Medela <nmedela@PMINTL.NET>
2019-05-28 15:42:44 -0300

    Se agrega test para birth y leaving Date

--- test/unit/dominio/doctor.spec.js
+++ test/unit/dominio/doctor.spec.js
@@ -85,31 +85,91 @@ describe('Doctor', () => {
         expect(doctor.provincialMatriculation).toEqual(provincialMatriculation)
 
     })
-    
-        it('when you set a valid entry date to a doctor it stores it like a moment', () => {
-            const entryDate = '01/01/1998'
-            expect(doctor.entryDate).toBeNull()
-            doctor.setEntryDate(entryDate)
-            expect(moment.isMoment(doctor.entryDate)).toBeTruthy()
-            expect(doctor.entryDate.format(formats.dateFormat)).toEqual(entryDate)
-        })
-        it('when you set an invalid entry date to a doctor it stores a null', () => {
-            const entryDate = '30/02/1998 23:00'
-            expect(doctor.entryDate).toBeNull()
-            doctor.setEntryDate(entryDate)
-            expect(doctor.entryDate).toBeNull()
-        })
-    
-        it('when you get the entry date from a prescription that has an entry date it returns a string representation', () => {
-            const entryDate = '01/01/1998'
-            doctor.setEntryDate(entryDate)
-            expect(doctor.getEntryDate()).toEqual(entryDate)
-        })
-    
-        it('when you get the entry date from a prescription that hasn´t an entry data it returns a null', () => {
-            doctor.setEntryDate(null)
-            expect(doctor.getEntryDate()).toBeNull()
-        })
 
-    //TODO: Egregar test para las otras fechas 
+
+
+    // Testing atributtes of date =D
+    it('when you set a valid birth date to a doctor it stores it like a moment', () => {
+        const birthDate = '13/04/1993'
+        expect(doctor.birthDate).toBeNull()
+        doctor.setBirthDate(birthDate)
+        expect(moment.isMoment(doctor.birthDate)).toBeTruthy()
+        expect(doctor.birthDate.format(formats.dateFormat)).toEqual(birthDate)
+    })
+
+    it('when you set a valid entry date to a doctor it stores it like a moment', () => {
+        const entryDate = '01/01/1998'
+        expect(doctor.entryDate).toBeNull()
+        doctor.setEntryDate(entryDate)
+        expect(moment.isMoment(doctor.entryDate)).toBeTruthy()
+        expect(doctor.entryDate.format(formats.dateFormat)).toEqual(entryDate)
+    })
+
+    it('when you set a valid leaving date to a doctor it stores it like a moment', () => {
+        const leavingDate = '01/01/1998'
+        expect(doctor.leavingDate).toBeNull()
+        doctor.setLeavingDate(leavingDate)
+        expect(moment.isMoment(doctor.leavingDate)).toBeTruthy()
+        expect(doctor.leavingDate.format(formats.dateFormat)).toEqual(leavingDate)
+    })
+
+
+
+    it('when you set an invalid birth date to a doctor it stores a null', () => {
+        const birthDate = '40/02/1998'
+        expect(doctor.birthDate).toBeNull()
+        doctor.setBirthDate(birthDate)
+        expect(doctor.birthDate).toBeNull()
+    })
+    it('when you set an invalid entry date to a doctor it stores a null', () => {
+        const entryDate = '30/02/1998 23:00'
+        expect(doctor.entryDate).toBeNull()
+        doctor.setEntryDate(entryDate)
+        expect(doctor.entryDate).toBeNull()
+    })
+
+    it('when you set an invalid leaving date to a doctor it stores a null', () => {
+        const leavingDate = '30/02/1998 23:00'
+        expect(doctor.leavingDate).toBeNull()
+        doctor.setLeavingDate(leavingDate)
+        expect(doctor.leavingDate).toBeNull()
+    })
+
+
+
+
+    it('when you get the birth date from a doctor that has an entry date it returns a string representation', () => {
+        const birthDate = '20/01/1998'
+        doctor.setBirthDate(birthDate)
+        expect(doctor.getBirthDate()).toEqual(birthDate)
+    })
+    it('when you get the entry date from a doctor that has an entry date it returns a string representation', () => {
+        const entryDate = '01/01/1998'
+        doctor.setEntryDate(entryDate)
+        expect(doctor.getEntryDate()).toEqual(entryDate)
+    })
+
+    it('when you get the leaving date from a doctor that has an leaving date it returns a string representation', () => {
+        const leavingDate = '09/02/1998'
+        doctor.setLeavingDate(leavingDate)
+        expect(doctor.getLeavingDate()).toEqual(leavingDate)
+    })
+
+
+
+
+    it('when you get the birth date from a doctor that hasn´t an birth data it returns a null', () => {
+        doctor.setBirthDate(null)
+        expect(doctor.getBirthDate()).toBeNull()
+    })
+
+    it('when you get the entry date from a doctor that hasn´t an entry data it returns a null', () => {
+        doctor.setEntryDate(null)
+        expect(doctor.getEntryDate()).toBeNull()
+    })
+    it('when you get the leaving date from a doctor that hasn´t an leaving data it returns a null', () => {
+        doctor.setLeavingDate(null)
+        expect(doctor.getLeavingDate()).toBeNull()
+    })
+    
 })
