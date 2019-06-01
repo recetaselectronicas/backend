91a01c9bd7cc304ef3bffbf627bb69b98e34a436
Guusy <guusygonzalo@gmail.com>
2019-06-01 18:08:19 -0300

    agrego test para cuando el repositorio de obras sociales falla

--- src/routes/medicalInsurance.js
+++ src/routes/medicalInsurance.js
@@ -1,9 +1,16 @@
-const express = require('express')
-const router = express.Router()
-const {MedicalInsuranceRepository} =  require('../repositories/medicalInsuranceRepository');
-router.get('/', async (req, res, next) => {
-    const medicalInsurances = await MedicalInsuranceRepository.getAll();
-    return res.status(200).send(medicalInsurances)
-})
+const express = require("express");
+const router = express.Router();
+const {
+  MedicalInsuranceRepository
+} = require("../repositories/medicalInsuranceRepository");
 
-module.exports = router
+router.get("/", async (req, res, next) => {
+  try {
+    const medicalInsurances = await MedicalInsuranceRepository.getAll();
+    return res.status(200).send(medicalInsurances);
+  } catch (error) {
+    return next(error);
+  }
+});
+
+module.exports = router;
--- test/e2e/routes/medicalInsurance.spec.js
+++ test/e2e/routes/medicalInsurance.spec.js
@@ -1,42 +1,63 @@
-const request = require('supertest');
-const {init} = require('../../../src/init/initServer');
-const {MedicalInsuranceRepository} = require('../../../src/repositories/medicalInsuranceRepository')
+const request = require("supertest");
+const { init } = require("../../../src/init/initServer");
+const {
+  MedicalInsuranceRepository
+} = require("../../../src/repositories/medicalInsuranceRepository");
 
 const app = init();
 
-describe('when do a get in /medical-insurance',()=>{
+describe("when do a get in /medical-insurance", () => {
+  describe("and the repository response ok", () => {
     const medicalInsurancesValue = [
-        {
-            id:0,
-            description:"OSDE"
-        },
-        {
-            id:1,
-            description:"SWISS MEDICAL"
-        }
-    ]
-    MedicalInsuranceRepository.medicalInsurances = medicalInsurancesValue
+      {
+        id: 0,
+        description: "OSDE"
+      },
+      {
+        id: 1,
+        description: "SWISS MEDICAL"
+      }
+    ];
 
-    it('return all avaiables the medicalInsurances',()=>{
-        return request(app)
-        .get('/medical-insurance')
+    beforeAll(() => {
+      MedicalInsuranceRepository.medicalInsurances = medicalInsurancesValue;
+    });
+
+    it("return all avaiables the medicalInsurances", () => {
+      return request(app)
+        .get("/medical-insurance")
         .expect(200)
-        .then(res =>  {
-            const firstMedicalInsurance = res.body[0]
-            expect(firstMedicalInsurance).toHaveProperty('id')
-            expect(firstMedicalInsurance).toHaveProperty('description')
-            expect(res.body).toEqual(medicalInsurancesValue)
+        .then(res => {
+          const firstMedicalInsurance = res.body[0];
+          expect(firstMedicalInsurance).toHaveProperty("id");
+          expect(firstMedicalInsurance).toHaveProperty("description");
+          expect(res.body).toEqual(medicalInsurancesValue);
         });
-    })
+    });
+  });
+
+  describe("and the repository fails on search", () => {
+    beforeAll(() => {
+      MedicalInsuranceRepository.getAll = () => {
+        throw {};
+      };
+    });
+
+    it("respond with 500 ", () => {
+      return request(app)
+        .get("/medical-insurance")
+        .expect(500);
+    });
+  });
 });
 
-describe('when do a get at /ping',()=>{
-    it('respond pong',()=>{
-        return request(app)
-        .get('/ping')
-        .expect(200)
-        .then((res)=>{
-            expect(res.body.message).toEqual("pong")
-        });
-    })
-})
+describe("when do a get at /ping", () => {
+  it("respond pong", () => {
+    return request(app)
+      .get("/ping")
+      .expect(200)
+      .then(res => {
+        expect(res.body.message).toEqual("pong");
+      });
+  });
+});
