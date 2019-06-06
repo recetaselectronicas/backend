0719dcab364e8c541e91ec94d5bf0c66b2f38837
nmedela <medela.nicolas@gmail.com>
2019-06-05 23:25:11 -0300

    cambio el json a testear de prescription para que arranque le item con null. y comienzo intento de test de item

--- src/domain/item.js
+++ src/domain/item.js
@@ -1,49 +1,80 @@
+const { dateTimeFormat } = require('../utils/utils')
+
 class Item {
     constructor() {
         this.id = null
         this.prescribed = {
-            quantity: 99,
+            quantity: null,
             medicine: {
-                id: 1
+                id: null
             }
         }
         this.received = {
-            quantity: 99,
-            soldDate: "12/12/12 10:10",
+            quantity: null,
+            soldDate: null,
             medicine: {
-                id: 1
+                id: null
             },
             pharmacist: {
-                id: 1
+                id: null
             }
         }
         this.audited = {
-            quantity: 99,
+            quantity: null,
             medicine: {
-                id: 1
+                id: null
             }
         }
     }
+    prescribe(quantity, medicine) {
+        this.prescribed = {
+            quantity: quantity,
+            medicine: { id: medicine.id },
+        }
+    }
 
+    receive(quantity, solDate, medicine, pharmacist) {
+        this.received = {
+            quantity: quantity,
+            solDate: dateTimeFormat.toDate(soldDate),
+            medicine: { id: medicine.id },
+            pharmacist: { id: pharmacist.id }
+        }
+    }
+    
+    audit(quantity, medicine) {
+        this.audit = {
+            quantity: quantity,
+            medicine: { id: medicine.id }
+        }
+    }
+
+    getSoldDate(){
+       return dateTimeFormat.toString(this.received.soldDate)
+    }
+    
     toJson() {
         return JSON.stringify(this)
     }
 
     static fromJson(json) {
-        if (!json){
+        if (!json) {
             return new Item()
         }
         let object = typeof json === 'object' ? json : JSON.parse(json)
         const item = new Item()
         item.id = object.id || item.id
+        item.prescribed= object.prescribed || item.prescribed
+        item.received = object.received || item.received
+        item.audited= object.audited || item.audited
         return item
     }
 
-    static fromObject(object){
-        if (!(object instanceof Item)){
+    static fromObject(object) {
+        if (!(object instanceof Item)) {
             return Item.fromJson(object)
         }
-        return object 
+        return object
     }
 }
 
