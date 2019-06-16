3597c0c9eca36962408476be931f15c9bcfd8715
Guusy <guusygonzalo@gmail.com>
2019-06-16 18:34:42 -0300

    se agrega el mapeo a nuevas tablas medicine, vademecum, coverage

--- src/repositories/tablesNames.js
+++ src/repositories/tablesNames.js
@@ -1,5 +1,10 @@
 module.exports = {
   MEDICAL_PRIMER: 'medical_primer',
   MEDICAL_INSURANCE: 'medical_insurance',
+  MEDICINE: 'medicine',
+  ITEM: 'item',
   DOCTOR: 'doctor',
+  VADEMECUM: 'vademecum',
+  COVERAGE: 'coverage',
+  COMPOSITION: 'composition',
 }
