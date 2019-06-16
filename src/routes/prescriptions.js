471dfb7b3884e855650679d11e184fd8b083e3fe
nmedela <medela.nicolas@gmail.com>
2019-06-16 18:25:33 -0300

    continuacion de maquina de estado. Pasar a cancelada

--- src/routes/prescriptions.js
+++ src/routes/prescriptions.js
@@ -6,6 +6,7 @@ const { StateMachine } = require('../state-machine/state-machine')
 const { newBadRequestError, isBusinessError } = require('../utils/errors')
 const { PrescriptionRepository } = require('../repositories/prescriptions-repository')
 const permissions = require('../permissions/identifiedUser')
+const errors = require('../utils/errors')
 
 router.post('/', (req, res, next) => {
   const { logger } = req.app.locals
@@ -22,6 +23,14 @@ router.post('/', (req, res, next) => {
 
 const secureMiddleware = (req, res, next) => {
   req.identifiedUser = permissions.getIdentifiedAffiliate(1)
+  return next()
+}
+const secureMiddleware2 = (req, res, next) => {
+  req.identifiedUser = permissions.getIdentifiedPharmacist(1)
+  return next()
+}
+const secureMiddleware3 = (req, res, next) => {
+  req.identifiedUser = permissions.getIdentifiedDoctor(1)
   return next()
 }
 
@@ -51,5 +60,60 @@ router.get('/:id', secureMiddleware, (req, res, next) => {
     })
     .catch(next)
 })
+
+router.put('/:id', secureMiddleware3, (req, res, next) => {
+  const { logger } = req.app.locals
+  const { identifiedUser } = req
+  const body = req.body
+  return PrescriptionRepository.getById(req.params.id)
+      .then((prescription) =>{
+        return toState[body.status].change(prescription, { ...body.data,identifiedUser })
+
+      })
+    .then(updatePrescription => res.status(201).json(updatePrescription.toPlainObject()))
+    .catch((err) => {
+      if (isBusinessError(err)) {
+        return next(newBadRequestError('Invalid prescription payload', err, 400))
+      }
+      return next(err)
+    })
+})
+
+
+const toState = {
+  CANCELLED: {
+    change : (prescription, data) => {
+      prescription.statusReason = data.reason
+      if (!data.identifiedUser.canCancel()) {
+        throw errors.newForbiddenResourceException("No puede cancelar la receta")
+      }
+      //TODO: Agregar en objeto de presciption el atributo statusReason
+      return StateMachine.toCancelled(prescription)
+    }
+  },
+  CONFIRMED: {
+    change : (prescription, data) => {
+      //TODO: Agregar en objeto de presciption el atributo statusReason
+      return StateMachine.toConfirmed(prescription)
+    }
+  },
+  RECEIVE: {
+    change : (prescription, data) => {
+      if (!data.identifiedUser.canReceive()) {
+        throw errors.newForbiddenResourceException("No puede recepcionar la receta")
+      }
+      data.items.forEach(item => {
+        let received = prescription.items.find(i=> i.id = item.id).received
+        received.medicine = item.medicine
+        received.quantity = item.quantity
+        received.pharmacist.id = data.identifiedUser.id
+        received.soldDate = item.soldDate
+      })
+      //TODO: Agregar en objeto de presciption el atributo statusReason
+      return StateMachine.toReceive(prescription)
+    }
+  }
+
+}
 
 module.exports = router
