9f9f7240a219a42891913e42adb46c39b356df7a
Leandro Devoto <leandro.devoto@mercadolibre.com>
2019-06-16 21:42:25 -0300

    Se saca un console.log

--- src/norms/metadata/rulesMetadata.js
+++ src/norms/metadata/rulesMetadata.js
@@ -1,8 +1,6 @@
 const { Rule } = require('../rules/rule')
 const { loadPredicate } = require('./metadata')
 
-console.log(loadPredicate)
-
 const RULE_METADATA = json => new Rule().initialize({
   ...json,
   predicate: loadPredicate(json)
