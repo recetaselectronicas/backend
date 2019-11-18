fa8cde27865eaaef97f344296eda84e625adee55
Gonzalo Gras Cantou <guusygonzalo@gmail.com>
2019-11-18 14:50:25 -0300

    Now can unlink a doctor from a medical insurance

--- src/repositories/medicalBookletRepository.js
+++ src/repositories/medicalBookletRepository.js
@@ -2,6 +2,7 @@
 const knex = require('../init/knexConnection')
 const { MEDICAL_BOOKLET, MEDICAL_INSURANCE } = require('./tablesNames')
 const { MedicalInsurance } = require('../domain/medicalInsurance')
+const { dateTimeFormat } = require('../utils/utils')
 
 class MedicalBookletRepository {
   getMedicalInsurancesFrom(idDoctor) {
@@ -9,8 +10,29 @@ class MedicalBookletRepository {
       .select()
       .table(MEDICAL_BOOKLET)
       .where({ idDoctor })
+      .whereNull('to_date')
       .leftJoin(MEDICAL_INSURANCE, `${MEDICAL_BOOKLET}.id_medical_insurance`, `${MEDICAL_INSURANCE}.id`)
       .then(response => response.map(medicalInsurance => MedicalInsurance.fromObject(medicalInsurance)))
+  }
+
+  getLink(idDoctor, idMedicalInsurance, datetime) {
+    return knex
+      .select()
+      .table(MEDICAL_BOOKLET)
+      .where({ idDoctor, idMedicalInsurance })
+      .whereRaw('(to_date is null or from_date <= ?)', [dateTimeFormat.toMysqlString(datetime)])
+      .first()
+  }
+
+  unlink(idDoctor, idMedicalInsurance, toDate) {
+    return knex
+      .table(MEDICAL_BOOKLET)
+      .update({
+        toDate
+      })
+      .where({
+        idDoctor, idMedicalInsurance
+      })
   }
 }
 
--- src/useCases/unliks/unlinkUsers.js
+++ src/useCases/unliks/unlinkUsers.js
@@ -2,10 +2,12 @@ const { userTypes } = require('../../permissions/identifiedUser')
 const { unlinkPatient } = require('./patient/unlinkPatient')
 const errors = require('../../utils/errors')
 const { unlinkMedicalInsurance } = require('./medicalInsurance/unlinkMedicalInsurance')
+const { unlinkDoctor } = require('./doctor/unlinkDoctor')
 
 const unlinkMap = {
   [userTypes.AFFILIATE]: unlinkPatient,
   [userTypes.MEDICAL_INSURANCE]: unlinkMedicalInsurance,
+  [userTypes.DOCTOR]: unlinkDoctor,
 }
 const unlinkUsers = async (user, body) => {
   if (unlinkMap[user.type]) {
